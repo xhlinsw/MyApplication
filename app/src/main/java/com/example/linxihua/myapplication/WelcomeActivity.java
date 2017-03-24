5fc04e26d4d2670787e88e4a578fc395aa4a41c7
linxihua <xhlin_sw@126.com>
2017-03-24 14:15:30 +0800

    第二次提交

---
+++ app/src/main/java/com/example/linxihua/myapplication/WelcomeActivity.java
@@ -0,0 +1,17 @@
+package com.example.linxihua.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import java.util.Date;
+
+public class WelcomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        Date date = new Date();
+
+    }
+}
